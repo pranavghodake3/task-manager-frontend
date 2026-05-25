a67c7d8f64808182fcec730d2d2ebc8e319acbfd
Pranav Ghodake <pranavghodake3@gmail.com>
2026-05-25 16:09:44 +0530

    Signup of User and Company Done

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,26 +1,33 @@
+import { useState } from "react";
 import "../assets/css/auth.css";
+import api from "../services/api";
 
 export default function Signup() {
+  const [selectedCompany, selectSelectedCompany] = useState("0");
+  const [companyName, selectCompanyName] = useState("");
+  const [firstName, selectFirstName] = useState("");
+  const [lastName, selectLastName] = useState("");
+  const [email, selectEmail] = useState("");
+  const [password, selectPassword] = useState("");
+  const [confirmPassword, selectConfirmPassword] = useState("");
+
+  function handleIsCompany(e) {
+    selectSelectedCompany(e.target.value);
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await api.post(selectedCompany == 1 ? "/auth/register/company" : "auth/register", {
+      ...(selectedCompany == 1 ? {name: companyName} : null),
+      firstName,
+      lastName,
+      email,
+      password
+    });
+    console.log(response);
+  }
   return (
     <div className="auth-page">
       <div className="auth-container">
-        {/* Left Side */}
-        {/* <div className="auth-left">
-          <h1>TaskFlow</h1>
-
-          <p>
-            Create your workspace and start managing projects like a professional
-            engineering team.
-          </p>
-
-          <div className="auth-features">
-            <div>✔ Project & Sprint Planning</div>
-            <div>✔ Team Collaboration</div>
-            <div>✔ Advanced Reporting</div>
-          </div>
-        </div> */}
-
-        {/* Right Side */}
         <div className="auth-right">
           <div className="auth-card">
             <h2>Create Account 🚀</h2>
@@ -29,25 +36,49 @@ export default function Signup() {
               Start building and managing your tasks today
             </p>
 
-            <form className="auth-form">
+            <form className="auth-form" onSubmit={handleSubmit}>
               <div className="form-group">
-                <label>Full Name</label>
-                <input type="text" placeholder="Enter your full name" />
+                <label>Is Company ?</label>
+                </div>
+              <div className="form-group">
+                <label>
+                  <input type="radio" name="company" checked={selectedCompany === "0"}  value="0" onChange={handleIsCompany} />
+                  No
+                </label>
+                <label>
+                  <input type="radio" name="company" checked={selectedCompany === "1"}  value="1" onChange={handleIsCompany} />
+                  Yes
+                </label>
+              </div>
+
+              <div className={`form-group ${ selectedCompany == '0' ? 'hide' : 'show' }`}>
+                <label>Company Name</label>
+                <input type="text" placeholder="Enter your company name" onChange={(e) => selectCompanyName(e.target.value)} />
+              </div>
+      
+              <div className="form-group">
+                <label>First Name</label>
+                <input type="text" placeholder="Enter your first name" onChange={(e) => selectFirstName(e.target.value)} />
+              </div>
+
+              <div className="form-group">
+                <label>Last Name</label>
+                <input type="text" placeholder="Enter your last name" onChange={(e) => selectLastName(e.target.value)} />
               </div>
 
               <div className="form-group">
                 <label>Email</label>
-                <input type="email" placeholder="Enter your email" />
+                <input type="email" placeholder="Enter your email" onChange={(e) => selectEmail(e.target.value)} />
               </div>
 
               <div className="form-group">
                 <label>Password</label>
-                <input type="password" placeholder="Create password" />
+                <input type="password" placeholder="Create password" onChange={(e) => selectPassword(e.target.value)} />
               </div>
 
               <div className="form-group">
                 <label>Confirm Password</label>
-                <input type="password" placeholder="Confirm password" />
+                <input type="password" placeholder="Confirm password" onChange={(e) => selectConfirmPassword(e.target.value)} />
               </div>
 
               <button type="submit" className="auth-btn">
